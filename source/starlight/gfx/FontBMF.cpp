88cbb11f3f4d9035e96bb8e403dd69fab36fd8e3
zetaPRIME <qmanxt@gmail.com>
2016-12-25 14:42:55 -0500

    wait, what? why did it do THAT of all things

--- source/starlight/gfx/FontBMF.cpp
+++ source/starlight/gfx/FontBMF.cpp
@@ -13,23 +13,23 @@ using starlight::gfx::FontBMF;
 
 #define err(nth, wat) *((unsigned int*)0x00100000+(nth))=wat;
 #define ded(wat) err(0,wat)
-Vector2 FFontBMF:Measure(std::string& text, float scale, float maxWidth) {
+Vector2 FontBMF::Measure(std::string& text, float scale, float maxWidth) {
     Vector2 v;
     PrintOp(Vector2(), text, scale, Color(), Vector2(), nullptr, maxWidth, &v);
     return v;
 }
 
-void FFontBMF:Print(Vector2 position, std::string& text, float scale, Color color, Vector2 justification, OptRef<Color> borderColor) {
+void FontBMF::Print(Vector2 position, std::string& text, float scale, Color color, Vector2 justification, OptRef<Color> borderColor) {
     PrintOp(position, text, scale, color, justification, borderColor, 2147483647, static_cast<Vector2*>(nullptr));
 }
 
-void FFontBMF:Print(VRect rect, std::string& text, float scale, Color color, Vector2 justification, OptRef<Color> borderColor) {
+void FontBMF::Print(VRect rect, std::string& text, float scale, Color color, Vector2 justification, OptRef<Color> borderColor) {
     if (borderColor && borderColor.get() != Color::transparent) rect = rect.Expand(-1, -1);
     Vector2 pos = rect.pos + rect.size * justification;
     PrintOp(pos, text, scale, color, justification, borderColor, rect.size.x, static_cast<Vector2*>(nullptr));
 }
 
-void FFontBMF:PrintOp(Vector2 position, std::string& text, float scale, const Color& color, Vector2 justification, OptRef<Color> borderColor, float maxWidth, Vector2* measure) {
+void FontBMF::PrintOp(Vector2 position, std::string& text, float scale, const Color& color, Vector2 justification, OptRef<Color> borderColor, float maxWidth, Vector2* measure) {
     float lineHeight = font->lineHeight * scale;
     float spaceWidth = font->Char(' ').advX * scale;
     
